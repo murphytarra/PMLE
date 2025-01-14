0b4abd74c09f54cf969c85ca4f6b08e27eace884
Charlotte Lane <charlottelane714@gmail.com>
2025-01-14 10:05:26 +0000

    testing again

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 print('hello')
 print('testing')
+
+def add(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a + b
+
+
+x = 2+3
