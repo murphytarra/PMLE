d5b59e0b423c2cd23fdc2108cd4a5218da4429ae
knirs <knirsad@gmail.com>
2025-01-14 09:57:40 +0000

    Example

---
+++ adam_example.py
@@ -0,0 +1,12 @@
+# Example project, write some code
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create some data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Plot the data
+plt.plot(x, y)
+plt.show()
