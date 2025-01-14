4a217fcf66d0182b7db59a6f0e8c72d1c4f33d4d
Henry-London <hjl59@cam.ac.uk>
2025-01-14 10:22:48 +0000

    added docstring to foo(x,y)

--- my_stuff/example.py
+++ my_stuff/example.py
@@ -1,4 +1,13 @@
 #comment
 
-def foo(x, y):
+def foo(x: int, y: int) -> str:
+    """just a silly little function
+
+    Args:
+        x (int): dummy variable
+        y (int): bogus number
+
+    Returns:
+        str: the word "bruh"
+    """
     return "bruh"
